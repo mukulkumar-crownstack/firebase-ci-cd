86c23e5adbc80e63d9f9d26a75f385c1ff060fa2
Mukul Kumar <mukul.kumar@crownstack.com>
2023-08-29 17:03:31 +0530

    added get prospect by id

--- functions/cloudAPI.f.js
+++ functions/cloudAPI.f.js
@@ -46,6 +46,20 @@ app.post(
   }
 );
 
+
+app.get("/truora/prospects/:id",
+  express.json({ type: "*/*" }),
+(req, res) => {
+  const phoneNumber  = req.params.phone;
+  admin.firestore().collection("driver_lead/leads/prospects").where("phone", "==", phoneNumber).limit(1).then(snapshot => {
+    if (docSnapshot.size > 0) {
+      res.status(200).json({isAvailable: false}); 
+    } else {
+      res.status(200).json({isAvailable: true}); 
+    }
+  });
+});
+
 app.post("/truora/prospects/add",
   express.json({ type: "*/*" }),
 (req, res) => {
