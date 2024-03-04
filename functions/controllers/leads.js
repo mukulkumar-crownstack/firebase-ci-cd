7381c1cf317306ed8a0ca696b7d2f0031365d992
Mukul Kumar <mukul.kumar@crownstack.com>
2024-03-04 16:02:37 +0530

    minor fix

--- functions/controllers/leads.js
+++ functions/controllers/leads.js
@@ -352,7 +352,7 @@ exports.putProspectStatus = async (req, res, next) => {
 };
 
 exports.postProspectQualify = async (req, res, next) => {
-    const { status, phone, created_by = "user" } = req.body;
+    const { status, phone, created_by = "user", add_dispatch_uuid } = req.body;
     let phoneNumber = helper_functions.getPhoneFromPhoneNumber(phone);
     const data = {
         status: status,
@@ -372,6 +372,9 @@ exports.postProspectQualify = async (req, res, next) => {
             const snapshot = await checkIfLeadAlreadyPresentAsQualified(phoneNumber, 'mx');
             if (snapshot.size === 0) {
                 prospectData['driver_uuid'] = prospectData.prospect_uuid;
+                if(prospectData.driver_type_code === 'cliente_independiente') {
+                    prospectData['dispatch_driver_uuid'] = prospectData.prospect_uuid;
+                }
                 const leadID = `${prospectData.driver_type_code}_${prospectData.phone_country_code}_${phoneNumber}`;
                 const qualifiedLeadDocPath = qulifiedleadDocPath.replace(':lead_uuid', leadID);
                 if (created_by && created_by !== 'admin') {
